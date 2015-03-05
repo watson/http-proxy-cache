b1c3de0b77f7a3f33ae30ea936fdc97be704f7f4
Thomas Watson Steen <w@tson.dk>
2015-03-05 11:10:38 +0100

    Add simple integration tests

---
+++ test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var http = require('http')
+var request = require('request')
+var afterAll = require('after-all')
+var test = require('tape')
+
+require('./')
+
+var count
+
+request = request.defaults({
+  proxy: 'http://localhost:' + process.env.PORT,
+  pool: { maxSockets: Infinity }
+})
+
+var server = http.createServer(function (req, res) {
+  setTimeout(function () {
+    count++
+    res.end()
+  }, 100)
+}).listen(function () {
+  var target = 'http://localhost:' + server.address().port
+
+  test('single connection', function (t) {
+    count = 0
+    request(target, function (err, res, body) {
+      t.error(err)
+      t.equal(count, 1)
+      t.end()
+    })
+  })
+
+  test('multiple non-blocking connections', function (t) {
+    count = 0
+    var total = 200
+    var start = Date.now()
+    var next = afterAll(function (err) {
+      var runtime = Date.now() - start
+      t.error(err)
+      t.equal(count, total)
+      t.ok(runtime < 1600, 'run time higher than expected: ' + runtime)
+      t.end()
+    })
+    for (var n = 1; n <= total; n++) request(target + '/' + n, next())
+  })
+
+  test('end', function (t) {
+    t.end()
+    process.exit()
+  })
+})
